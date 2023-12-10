2de819de190429fc60de032d3b60eff1ed2aa12f
Volodymyr the destroyer of dwarf <152634784+VladtheGeneral@users.noreply.github.com>
2023-12-09 21:30:01 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            # . . . #
+            . # # # .
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+        basic.pause(1000)
+    }
+})
+basic.forever(function () {
+	
+})
